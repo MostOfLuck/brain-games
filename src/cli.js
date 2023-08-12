57f80052de66d5549be635b082078f402e999c50
Sam <burtman.samuel26@gmail.com>
2023-08-12 17:07:35 +0300

    src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const runCli = () => {
-  console.log("Welcome to the Brain Games!");
+  console.log('Welcome to the Brain Games!');
 
-  const playerName = readlineSync.question("May I have your name? ");
+  const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
 };
 
