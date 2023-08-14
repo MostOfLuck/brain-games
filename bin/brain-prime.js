6aeb462bf6dc00c0ac1c4141aecd0e5899aa00c5
Sam <burtman.samuel26@gmail.com>
2023-08-14 14:44:43 +0300

    add new game brain-prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const isPrime = (num) => {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const playBrainPrimeGame = () => {
+  const gameRound = () => {
+    const number = Math.floor(Math.random() * 100) + 1;
+    const question = number;
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+
+    return { question, correctAnswer };
+  };
+
+  console.log('Welcome to the Brain Games!');
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  let correctAnswers = 0;
+
+  while (correctAnswers < 3) {
+    const { question, correctAnswer } = gameRound();
+
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswers += 1;
+    } else {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+      );
+      console.log(`Let's try again, ${playerName}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${playerName}!`);
+};
+
+playBrainPrimeGame();
