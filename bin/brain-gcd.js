7d561f43a941ce6dac74b6972b5d82ca9fb9325b
Sam <burtman.samuel26@gmail.com>
2023-08-17 00:48:35 +0300

    refactoting brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -13,37 +13,34 @@ const getGCD = (num1, num2) => {
 
   return a;
 };
+console.log('Welcome to the Brain Games!');
+const playerName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${playerName}!`);
+console.log('Find the greatest common divisor of given numbers.');
 
-const playGCDGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const playerName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!`);
-  console.log('Find the greatest common divisor of given numbers.');
+let correctAnswers = 0;
 
-  let correctAnswers = 0;
+while (correctAnswers < 3) {
+  const number1 = Math.floor(Math.random() * 100) + 1;
+  const number2 = Math.floor(Math.random() * 100) + 1;
+  const question = `${number1} ${number2}`;
+  const correctAnswer = String(getGCD(number1, number2));
 
-  while (correctAnswers < 3) {
-    const number1 = Math.floor(Math.random() * 100) + 1;
-    const number2 = Math.floor(Math.random() * 100) + 1;
-    const question = `${number1} ${number2}`;
-    const correctAnswer = String(getGCD(number1, number2));
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ');
 
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-      correctAnswers += 1;
-    } else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
-      );
-      console.log(`Let's try again, ${playerName}!`);
-      return;
-    }
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    correctAnswers += 1;
+  } else {
+    console.log(
+      `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+    );
+    console.log(`Let's try again, ${playerName}!`);
+    break;
   }
+}
 
+if (correctAnswers === 3) {
   console.log(`Congratulations, ${playerName}!`);
-};
-
-playGCDGame();
+}
