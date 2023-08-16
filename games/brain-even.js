dd2e79dcdd538748db38b64c2460714486192a42
Sam <burtman.samuel26@gmail.com>
2023-08-17 00:54:18 +0300

    refactoring brain-even

--- games/brain-even.js
+++ games/brain-even.js
@@ -3,38 +3,32 @@ import readlineSync from 'readline-sync';
 
 const isEven = (num) => num % 2 === 0;
 
-const playEvenGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const playerName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log('Welcome to the Brain Games!');
+const playerName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${playerName}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  let correctAnswers = 0;
+let correctAnswers = 0;
 
-  while (correctAnswers < 3) {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    console.log(`Question: ${randomNumber}`);
-    const userAnswer = readlineSync.question('Your answer: ');
+while (correctAnswers < 3) {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  console.log(`Question: ${randomNumber}`);
+  const userAnswer = readlineSync.question('Your answer: ');
 
-    if (
-      // eslint-disable-next-line operator-linebreak
-      (isEven(randomNumber) && userAnswer === 'yes') ||
-      (!isEven(randomNumber) && userAnswer === 'no')
-    ) {
-      console.log('Correct!');
-      correctAnswers += 1;
-    } else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${
-          isEven(randomNumber) ? 'yes' : 'no'
-        }'.`,
-      );
-      console.log(`Let's try again, ${playerName}!`);
-      return;
-    }
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    correctAnswers += 1;
+  } else {
+    console.log(
+      `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+    );
+    console.log(`Let's try again, ${playerName}!`);
+    break;
   }
+}
 
+if (correctAnswers === 3) {
   console.log(`Congratulations, ${playerName}!`);
-};
-
-playEvenGame();
+}
