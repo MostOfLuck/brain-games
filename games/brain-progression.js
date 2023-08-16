8d3258349c9f67d0f939bb40c509252eb5b44f40
Sam <burtman.samuel26@gmail.com>
2023-08-17 00:40:20 +0300

    refactoring brain-progression.js

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -9,51 +9,39 @@ const generateProgression = (start, step, length) => {
   return progression;
 };
 
-const askForName = () => {
-  console.log('Welcome to the Brain Games!');
-  const playerName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!`);
-  return playerName;
-};
+console.log('Welcome to the Brain Games!');
+const playerName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${playerName}!`);
+console.log('What number is missing in the progression?');
 
-const showProgressionGameIntro = () => {
-  const playerName = askForName();
-  console.log('What number is missing in the progression?');
-  return playerName;
-};
+let correctAnswers = 0;
 
-const playProgressionGame = () => {
-  const playerName = showProgressionGameIntro();
+while (correctAnswers < 3) {
+  const start = Math.floor(Math.random() * 50) + 1;
+  const step = Math.floor(Math.random() * 10) + 1;
+  const progressionLength = 10;
+  const hiddenIndex = Math.floor(Math.random() * progressionLength);
 
-  let correctAnswers = 0;
+  const progression = generateProgression(start, step, progressionLength);
+  const correctAnswer = progression[hiddenIndex];
+  progression[hiddenIndex] = '..';
+  const question = progression.join(' ');
 
-  while (correctAnswers < 3) {
-    const start = Math.floor(Math.random() * 50) + 1;
-    const step = Math.floor(Math.random() * 10) + 1;
-    const progressionLength = 10;
-    const hiddenIndex = Math.floor(Math.random() * progressionLength);
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ');
 
-    const progression = generateProgression(start, step, progressionLength);
-    const correctAnswer = progression[hiddenIndex];
-    progression[hiddenIndex] = '..';
-    const question = progression.join(' ');
-
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer === String(correctAnswer)) {
-      console.log('Correct!');
-      correctAnswers += 1;
-    } else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
-      );
-      console.log(`Let's try again, ${playerName}!`);
-      return;
-    }
+  if (userAnswer === String(correctAnswer)) {
+    console.log('Correct!');
+    correctAnswers += 1;
+  } else {
+    console.log(
+      `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+    );
+    console.log(`Let's try again, ${playerName}!`);
+    break;
   }
+}
 
+if (correctAnswers === 3) {
   console.log(`Congratulations, ${playerName}!`);
-};
-
-playProgressionGame();
+}
