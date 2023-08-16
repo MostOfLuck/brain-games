f2c926939860bef71ab56e69e42adb9150ce54e3
Sam <burtman.samuel26@gmail.com>
2023-08-16 23:58:35 +0300

    update brain-calc.js bin and games directories.

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,20 +1,29 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-const playCalcGame = () => {
+const generateExpression = () => {
+  const num1 = Math.floor(Math.random() * 100);
+  const num2 = Math.floor(Math.random() * 100);
+  const operators = ['+', '-', '*'];
+  const operator = operators[Math.floor(Math.random() * operators.length)];
+  return { num1, num2, operator };
+};
+
+const askForName = () => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
+  return playerName;
+};
+
+const playCalcGame = () => {
+  const playerName = askForName();
   console.log('What is the result of the expression?');
 
   let correctAnswers = 0;
 
   while (correctAnswers < 3) {
-    const num1 = Math.floor(Math.random() * 100);
-    const num2 = Math.floor(Math.random() * 100);
-    const operators = ['+', '-', '*'];
-    const operator = operators[Math.floor(Math.random() * operators.length)];
-
+    const { num1, num2, operator } = generateExpression();
     const expression = `${num1} ${operator} ${num2}`;
     const correctAnswer = eval(expression).toString();
 
@@ -28,7 +37,7 @@ const playCalcGame = () => {
       console.log(
         `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
       );
-      console.log("Let's try again!");
+      console.log(`Let's try again, ${playerName}!`);
       return;
     }
   }
