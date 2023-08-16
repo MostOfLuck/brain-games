0d64d18ad173d88999ee07ddcaaeafee17d76f7d
Sam <burtman.samuel26@gmail.com>
2023-08-17 01:06:08 +0300

    refactoring brain-progresion

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -17,21 +17,27 @@ const showProgressionGameIntro = () => {
   return playerName;
 };
 
+const generateQuestion = () => {
+  const start = Math.floor(Math.random() * 50) + 1;
+  const step = Math.floor(Math.random() * 10) + 1;
+  const progressionLength = 10;
+  const hiddenIndex = Math.floor(Math.random() * progressionLength);
+
+  const progression = generateProgression(start, step, progressionLength);
+  const correctAnswer = progression[hiddenIndex];
+  progression[hiddenIndex] = '..';
+  const question = progression.join(' ');
+
+  return { question, correctAnswer };
+};
+
 const playProgressionGame = () => {
   const playerName = showProgressionGameIntro();
 
   let correctAnswers = 0;
 
   while (correctAnswers < 3) {
-    const start = Math.floor(Math.random() * 50) + 1;
-    const step = Math.floor(Math.random() * 10) + 1;
-    const progressionLength = 10;
-    const hiddenIndex = Math.floor(Math.random() * progressionLength);
-
-    const progression = generateProgression(start, step, progressionLength);
-    const correctAnswer = progression[hiddenIndex];
-    progression[hiddenIndex] = '..';
-    const question = progression.join(' ');
+    const { question, correctAnswer } = generateQuestion();
 
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
