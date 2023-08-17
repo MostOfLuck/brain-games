3d3599d69a895a112f1b41fa350371ff9ab4ba28
Sam <burtman.samuel26@gmail.com>
2023-08-18 00:19:20 +0300

    add export to function greetPlayer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-const greetPlayer = () => {
+export const greetPlayer = () => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
