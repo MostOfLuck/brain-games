ac856510fc4ac533c4ee041b99cea6bfb17ef77a
Sam <burtman.samuel26@gmail.com>
2023-08-18 00:46:34 +0300

    refactoring brain-progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import { playGame, checkCalcAnswer } from '../gameUtils.js';
 
 const generateProgression = (start, step, length) => {
   const progression = [];
@@ -9,54 +9,19 @@ const generateProgression = (start, step, length) => {
   return progression;
 };
 
-const showProgressionGameIntro = () => {
-  console.log('Welcome to the Brain Games!');
-  const playerName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!`);
-  console.log('What number is missing in the progression?');
-  return playerName;
-};
-
-const generateQuestion = () => {
+const generateQuestionAndAnswer = () => {
   const start = Math.floor(Math.random() * 50) + 1;
   const step = Math.floor(Math.random() * 10) + 1;
   const progressionLength = 10;
   const hiddenIndex = Math.floor(Math.random() * progressionLength);
 
   const progression = generateProgression(start, step, progressionLength);
-  const correctAnswer = progression[hiddenIndex];
+  const correctAnswer = String(progression[hiddenIndex]);
   progression[hiddenIndex] = '..';
   const question = progression.join(' ');
 
   return { question, correctAnswer };
 };
 
-const playProgressionGame = () => {
-  const playerName = showProgressionGameIntro();
-
-  let correctAnswers = 0;
-
-  while (correctAnswers < 3) {
-    const { question, correctAnswer } = generateQuestion();
-
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer === String(correctAnswer)) {
-      console.log('Correct!');
-      correctAnswers += 1;
-    } else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
-      );
-      console.log(`Let's try again, ${playerName}!`);
-      break;
-    }
-  }
-
-  if (correctAnswers === 3) {
-    console.log(`Congratulations, ${playerName}!`);
-  }
-};
-
-playProgressionGame();
+const progressionQuestionText = 'What number is missing in the progression?';
+playGame(progressionQuestionText, generateQuestionAndAnswer, checkCalcAnswer);
