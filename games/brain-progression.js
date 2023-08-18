8fc57dec4651ff51b5fc418e11aa9484dc0dcdf6
Sam <burtman.samuel26@gmail.com>
2023-08-18 13:27:34 +0300

    refactoring games brain-progression

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -1,27 +1,6 @@
 #!/usr/bin/env node
 import { playGame, checkCalcAnswer } from '../gameUtils.js';
-
-const generateProgression = (start, step, length) => {
-  const progression = [];
-  for (let i = 0; i < length; i += 1) {
-    progression.push(start + step * i);
-  }
-  return progression;
-};
-
-const generateQuestionAndAnswer = () => {
-  const start = Math.floor(Math.random() * 50) + 1;
-  const step = Math.floor(Math.random() * 10) + 1;
-  const progressionLength = 10;
-  const hiddenIndex = Math.floor(Math.random() * progressionLength);
-
-  const progression = generateProgression(start, step, progressionLength);
-  const correctAnswer = String(progression[hiddenIndex]);
-  progression[hiddenIndex] = '..';
-  const question = progression.join(' ');
-
-  return { question, correctAnswer };
-};
+import generateQuestionAndAnswer from '../progressionGameUtils.js';
 
 const progressionQuestionText = 'What number is missing in the progression?';
 playGame(progressionQuestionText, generateQuestionAndAnswer, checkCalcAnswer);
