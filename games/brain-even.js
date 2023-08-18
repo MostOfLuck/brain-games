7c8769fe9a16acf155660640545ab7e73c017ed0
Sam <burtman.samuel26@gmail.com>
2023-08-18 14:16:32 +0300

    refactoring update brain-even bin and games directrories

--- games/brain-even.js
+++ games/brain-even.js
@@ -7,4 +7,5 @@ const isEven = (num) => num % 2 === 0;
 console.log('Welcome to the Brain Games!');
 const playerName = readlineSync.question('May I have your name? ');
 
+console.log(`Hello, ${playerName}!`);
 playEvenGame(playerName, isEven);
