b89958529c2d3d5078a563d7c0794140c1ff533c
Sam <burtman.samuel26@gmail.com>
2023-08-18 18:37:57 +0300

    refactoring games/brain-calc.js

--- games/brain-calc.js
+++ games/brain-calc.js
@@ -1,13 +1,6 @@
 #!/usr/bin/env node
 import { playGame, checkCalcAnswer } from '../gameUtils.js';
-import { generateExpression, calculate } from '../calcUtils.js';
-
-const generateQuestionAndAnswer = () => {
-  const { num1, num2, operator } = generateExpression();
-  const question = `${num1} ${operator} ${num2}`;
-  const correctAnswer = calculate(num1, operator, num2).toString();
-  return { question, correctAnswer };
-};
+import generateQuestionAndAnswer from '../calcUtils.js';
 
 playGame(
   'What is the result of the expression?',
