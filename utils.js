c974fe28baffdd4e644c35f21f222eaa222f752a
Sam <burtman.samuel26@gmail.com>
2023-08-18 18:48:36 +0300

    add new file to support all programms

---
+++ utils.js
@@ -0,0 +1,11 @@
+// utils.js
+import readlineSync from 'readline-sync';
+
+const greetPlayer = () => {
+  console.log('Welcome to the Brain Games!');
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+  return playerName;
+};
+
+export default greetPlayer;
