383205d0a445f8d394a407990aa2ebc9f68a4202
Sam <burtman.samuel26@gmail.com>
2023-08-18 14:07:45 +0300

    refactoring brain-even in games directory

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,34 +1,10 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import playEvenGame from '../evenGameUtils.js';
 
 const isEven = (num) => num % 2 === 0;
 
 console.log('Welcome to the Brain Games!');
 const playerName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${playerName}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-let correctAnswers = 0;
-
-while (correctAnswers < 3) {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
-  console.log(`Question: ${randomNumber}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-
-  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-    correctAnswers += 1;
-  } else {
-    console.log(
-      `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
-    );
-    console.log(`Let's try again, ${playerName}!`);
-    break;
-  }
-}
-
-if (correctAnswers === 3) {
-  console.log(`Congratulations, ${playerName}!`);
-}
+playEvenGame(playerName, isEven);
