5776c359001b9f843ffab5fa2ffad4772f95e6ce
Sam <burtman.samuel26@gmail.com>
2023-08-18 16:03:25 +0300

    add new file to support brain-prime

---
+++ primeUtils.js
@@ -0,0 +1,13 @@
+const isPrime = (num) => {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export default isPrime;
