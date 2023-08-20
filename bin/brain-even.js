ebd8b6f7f307dff24f3bf951cb4cbced13044f2e
Sam <burtman.samuel26@gmail.com>
2023-08-20 23:54:31 +0300

    update bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import playEvenGame from '../evenGameUtils.js';
-
-const isEven = (num) => num % 2 === 0;
+import playEvenGame from '../src/games/brain-even.js';
 
 console.log('Welcome to the Brain Games!');
 const playerName = readlineSync.question('May I have your name? ');
 
 console.log(`Hello, ${playerName}!`);
-playEvenGame(playerName, isEven);
+playEvenGame(playerName);
