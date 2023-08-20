d37269abe3d9478af6edc2a5c18b8af36b5dd08b
Sam <burtman.samuel26@gmail.com>
2023-08-20 23:59:32 +0300

    add new file to check answers src/answerCheckers.js

---
+++ src/answerCheckers.js
@@ -0,0 +1,3 @@
+export const checkCalcAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
+export const checkPrimeAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
+export const checkProgressionAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
