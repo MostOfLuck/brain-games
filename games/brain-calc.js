b2b6a8ca01447e683eb68712cf15e6f5778554d6
Sam <burtman.samuel26@gmail.com>
2023-08-21 13:52:02 +0300

    update

--- games/brain-calc.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { playGame, checkCalcAnswer } from '../gameUtils.js';
-import generateQuestionAndAnswer from '../calcUtils.js';
-
-playGame(
-  'What is the result of the expression?',
-  generateQuestionAndAnswer,
-  checkCalcAnswer,
-);
--- games/brain-prime.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import { playGame, checkCalcAnswer } from '../gameUtils.js';
-import generateQuestionAndAnswer from '../primeGameUtils.js';
-
-const primeQuestionText = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-playGame(primeQuestionText, generateQuestionAndAnswer, checkCalcAnswer);
