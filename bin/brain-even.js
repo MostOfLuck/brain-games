8c9234068c0aaa05f7687fe5ab12fa25ed1d984d
Sam <burtman.samuel26@gmail.com>
2023-08-21 13:48:17 +0300

    moved calcUtils.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
 import playEvenGame from '../src/games/brain-even.js';
 
-console.log('Welcome to the Brain Games!');
-const playerName = readlineSync.question('May I have your name? ');
-
-console.log(`Hello, ${playerName}!`);
-playEvenGame(playerName);
+playEvenGame();
