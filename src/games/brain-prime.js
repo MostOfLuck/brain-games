784bc82dc05e657da23de5f46d3a72fc54c3359a
Sam <burtman.samuel26@gmail.com>
2023-08-22 13:19:38 +0300

    update src/games/brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-import { playGame } from '../utils/gameUtils.js';
+import { playGame, greetPlayer } from '../utils/gameUtils.js';
 import generateQuestionAndAnswer from '../utils/primeGameUtils.js';
 
 const primeQuestionText = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-playGame(primeQuestionText, generateQuestionAndAnswer);
+const playerName = greetPlayer();
+playGame(primeQuestionText, generateQuestionAndAnswer, playerName);
+
+export default playGame;
