5e167409e1c0ef268c8324c1bdb2152a108269ec
Sam <burtman.samuel26@gmail.com>
2023-08-22 13:44:24 +0300

    update bin/brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,2 @@
 #!/usr/bin/env node
-import { playGame, greetPlayer } from '../src/utils/gameUtils.js';
 import '../src/games/brain-progression.js';
-
-const playerName = greetPlayer();
-playGame(playerName);
