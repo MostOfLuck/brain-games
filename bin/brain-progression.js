a4d09377f49bba1a52dac091b8b75efbb99ab322
Sam <burtman.samuel26@gmail.com>
2023-08-22 13:23:00 +0300

    update bin/brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import { playGame, greetPlayer } from '../src/utils/gameUtils.js';
-import { generateQuestionAndAnswer } from '../src/utils/progressionGameUtils.js';
+import '../src/games/brain-progression.js';
 
-const progressionQuestionText = 'What number is missing in the progression?';
 const playerName = greetPlayer();
-playGame(progressionQuestionText, generateQuestionAndAnswer, playerName);
+playGame(playerName);
