da46dda3ae4c86f7afa7416ae79cde04e7b12968
Sam <burtman.samuel26@gmail.com>
2023-08-22 14:00:58 +0300

    update src/games/brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,7 +2,7 @@
 import { playGame, greetPlayer } from '../utils/gameUtils.js';
 import { generateQuestionAndAnswer } from '../utils/progressionGameUtils.js';
 
-const gameQuestionText = 'Your game question text here';
+const gameQuestionText = 'What number is missing in the progression?';
 const playerName = greetPlayer();
 playGame(gameQuestionText, generateQuestionAndAnswer, playerName);
 
