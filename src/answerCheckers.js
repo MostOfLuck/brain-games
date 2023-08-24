5a028172bb3aacec1d0bdbb0dab7173508698ef4
Sam <burtman.samuel26@gmail.com>
2023-08-24 21:24:22 +0300

    delete unneeded file src/answerCheckers.js

--- src/answerCheckers.js
+++
@@ -1,3 +0,0 @@
-export const checkCalcAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
-export const checkPrimeAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
-export const checkProgressionAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
