b059d07174ecfe5c11ca3f612e7077b58bceeb8f
Sam <burtman.samuel26@gmail.com>
2023-08-25 20:05:34 +0300

    update  src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
-import greetPlayer from './utils/utils.js';
+import readlineSync from 'readline-sync';
+
+const greetPlayer = () => {
+  console.log('Welcome to the Brain Games!');
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+  return playerName;
+};
 
 const runGame = (gameFunction) => {
   const playerName = greetPlayer();
