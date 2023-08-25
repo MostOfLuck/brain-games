0cc362f7669ff116c97ad4feb662cc1f1b590321
Sam <burtman.samuel26@gmail.com>
2023-08-25 19:47:51 +0300

    update src/games/brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,6 +1,33 @@
 #!/usr/bin/env node
 import { playGame, greetPlayer } from '../utils/gameUtils.js';
-import generateQuestionAndAnswer from '../utils/calcUtils.js';
+
+const generateExpression = () => {
+  const num1 = Math.floor(Math.random() * 100);
+  const num2 = Math.floor(Math.random() * 100);
+  const operators = ['+', '-', '*'];
+  const operator = operators[Math.floor(Math.random() * operators.length)];
+  return { num1, num2, operator };
+};
+
+const calculate = (num1, operator, num2) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error(`Unknown operator: ${operator}`);
+  }
+};
+
+const generateQuestionAndAnswer = () => {
+  const { num1, num2, operator } = generateExpression();
+  const question = `${num1} ${operator} ${num2}`;
+  const correctAnswer = calculate(num1, operator, num2).toString();
+  return { question, correctAnswer };
+};
 
 const playCalcGame = () => {
   const playerName = greetPlayer();
