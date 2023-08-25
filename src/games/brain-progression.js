fa9e9eb41d2539d94504154ac030601a96f877f2
Sam <burtman.samuel26@gmail.com>
2023-08-25 19:56:21 +0300

    update src/games/brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,6 +1,27 @@
-#!/usr/bin/env node
+// c#!/usr/bin/env node
 import { playGame, greetPlayer } from '../utils/gameUtils.js';
-import { generateQuestionAndAnswer } from '../utils/progressionGameUtils.js';
+
+const generateProgression = (start, step, length) => {
+  const progression = [];
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + step * i);
+  }
+  return progression;
+};
+
+export const generateQuestionAndAnswer = () => {
+  const start = Math.floor(Math.random() * 50) + 1;
+  const step = Math.floor(Math.random() * 10) + 1;
+  const progressionLength = 10;
+  const hiddenIndex = Math.floor(Math.random() * progressionLength);
+
+  const progression = generateProgression(start, step, progressionLength);
+  const correctAnswer = String(progression[hiddenIndex]);
+  progression[hiddenIndex] = '..';
+  const question = progression.join(' ');
+
+  return { question, correctAnswer };
+};
 
 const gameQuestionText = 'What number is missing in the progression?';
 const playerName = greetPlayer();
