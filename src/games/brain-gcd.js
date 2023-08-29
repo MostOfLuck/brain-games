c6fa9a54baf2f68fca616d4335a6de04a72707eb
Sam <burtman.samuel26@gmail.com>
2023-08-29 21:50:59 +0300

    update src/games/brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,4 +1,4 @@
-// src/games/brain-gcd.js
+#!/usr/bin/env node
 import { playGame, greetPlayer } from '../utils/gameUtils.js';
 import { generateRandomNumber } from '../index.js';
 
@@ -15,19 +15,20 @@ const getGCD = (num1, num2) => {
   return a;
 };
 
-const generateGCDQuestion = () => {
-  const number1 = generateRandomNumber(1, 100);
-  const number2 = generateRandomNumber(1, 100);
-  const question = `${number1} ${number2}`;
-  const correctAnswer = String(getGCD(number1, number2));
-
-  return { question, correctAnswer };
-};
-
 const playGcdGame = () => {
+  greetPlayer();
   const gcdQuestionText = 'Find the greatest common divisor of given numbers.';
-  const playerName = greetPlayer();
-  playGame(gcdQuestionText, generateGCDQuestion, playerName);
+
+  const generateGCDQuestion = () => {
+    const number1 = generateRandomNumber(1, 100);
+    const number2 = generateRandomNumber(1, 100);
+    const question = `${number1} ${number2}`;
+    const correctAnswer = String(getGCD(number1, number2));
+
+    return { question, correctAnswer };
+  };
+
+  playGame(gcdQuestionText, generateGCDQuestion);
 };
 
 export default playGcdGame;
