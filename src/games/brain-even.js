8385c72ec7b187f832767b9ce9b0e25a93286e86
Sam <burtman.samuel26@gmail.com>
2023-08-29 21:50:47 +0300

    update src/games/brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
-// src/games/brain-even.js
 import { playGame, greetPlayer } from '../utils/gameUtils.js';
 import { generateRandomNumber } from '../index.js';
 
 const isEven = (number) => number % 2 === 0;
 
-const generateQuestionAndAnswer = () => {
-  const question = generateRandomNumber(1, 100);
-  const correctAnswer = isEven(question) ? 'yes' : 'no';
-  return { question, correctAnswer };
-};
-
 const playEvenGame = () => {
+  greetPlayer();
   const introMessage = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const playerName = greetPlayer();
-  playGame(introMessage, generateQuestionAndAnswer, playerName);
+
+  const generateQuestionAndAnswer = () => {
+    const question = generateRandomNumber(1, 100);
+    const correctAnswer = isEven(question) ? 'yes' : 'no';
+    return { question, correctAnswer };
+  };
+
+  playGame(introMessage, generateQuestionAndAnswer);
 };
 
 export default playEvenGame;
