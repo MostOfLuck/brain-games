37a0dacf545c9928027bfab6309cfdef0d9dc5fe
Sam <burtman.samuel26@gmail.com>
2023-09-02 12:57:14 +0300

    The engene of the all games was moved from gameUtils to index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-const greetPlayer = () => {
-  console.log('Welcome to the Brain Games!');
-  const playerName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!`);
-  return playerName;
-};
-
 export const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+let playerName = null;
+
+export const greetPlayer = () => {
+  console.log('Welcome to the Brain Games!');
+  playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+};
+
 const runGame = (gameFunction) => {
-  const playerName = greetPlayer();
+  greetPlayer();
   gameFunction(playerName);
 };
 
+export const playGame = (introMessage, generateQuestionAndAnswer) => {
+  console.log(introMessage);
+
+  let correctAnswers = 0;
+
+  while (correctAnswers < 3) {
+    const { question, correctAnswer } = generateQuestionAndAnswer();
+    const userAnswer = readlineSync.question(
+      `Question: ${question}\nYour answer: `,
+    );
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswers += 1;
+    } else {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+      );
+      console.log(`Let's try again, ${playerName}!`);
+      process.exit(1);
+    }
+  }
+
+  console.log(`Congratulations, ${playerName}!`);
+  process.exit(0);
+};
 export default runGame;
