9b5b61a66d6883f2e9adf3e37b78606361a6c20f
Sam <burtman.samuel26@gmail.com>
2023-09-05 20:36:52 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,39 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
 export const playGame = (introMessage, generateQuestionAndAnswer) => {
+  let playerName = null;
+
   const greetPlayer = () => {
     console.log('Welcome to the Brain Games!');
-    const playerName = readlineSync.question('May I have your name? ');
+    playerName = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${playerName}!`);
-    return playerName;
   };
 
-  const displayQuestion = (question) => readlineSync.question(`Question: ${question}\nYour answer: `);
-
-  const displayResult = (isCorrect, correctAnswer, playerName) => {
-    if (isCorrect) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${correctAnswer}' is the correct answer.`);
-      console.log(`Let's try again, ${playerName}!`);
-      process.exit(1);
-    }
-  };
-
-  const getPlayerName = greetPlayer();
+  greetPlayer();
   console.log(introMessage);
 
   let correctAnswers = 0;
 
-  const checkAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
-
   while (correctAnswers < 3) {
     const { question, correctAnswer } = generateQuestionAndAnswer();
-    const userAnswer = displayQuestion(question);
+    const userAnswer = readlineSync.question(
+      `Question: ${question}\nYour answer: `,
+    );
 
-    const isCorrect = checkAnswer(userAnswer, correctAnswer);
-    displayResult(isCorrect, correctAnswer, getPlayerName);
-
-    correctAnswers += isCorrect ? 1 : 0;
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswers += 1;
+    } else {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+      );
+      console.log(`Let's try again, ${playerName}!`);
+      process.exit(0);
+    }
   }
 
-  console.log(`Congratulations, ${getPlayerName}!`);
+  console.log(`Congratulations, ${playerName}!`);
   process.exit(0);
 };
-
 export default playGame;
